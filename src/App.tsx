e4e3013a30b380af28c417dfa6ec3bee100dfcd0
lirbre <brenoliradev@gmail.com>
2022-11-22 22:36:02 -0300

    feat: modal with zustand & styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Button } from './components'
+import { useModalStore } from './stores/modal-store';
+
 function App() {
+  const open = useModalStore(state => state.open)
+  const updateModalContent = useModalStore(state => state.updateModalContent)
+
+  const handleOpenForm = () => {
+    updateModalContent([{
+      modalBody: <form id="test-form-id" onSubmit={(e) => {
+        e.preventDefault()
+        alert('submit')
+      }} />,
+      modalHeader: 'Adicionar novo funcionário',
+      cancelText: 'Cancelar',
+      confirmText: 'Adicionar',
+      formId: 'test-form-id'
+    }])
+
+    open()
+  }
+
+  const handleOpenSimple = () => {
+    updateModalContent([{
+      modalBody: <div>test</div>,
+      modalHeader: 'Adicionar novo funcionário',
+      cancelText: 'Cancelar',
+      confirmText: 'Adicionar'
+    },{
+      modalBody: <div>test 2</div>,
+      modalHeader: 'Adicionar novo funcionário 2',
+      cancelText: 'Cancelar 2',
+      confirmText: 'Adicionar 2'
+    }])
+
+    open()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={handleOpenForm} btnColor='transparent' text={'Open Form'} textColor='#000' />
+      <Button onClick={handleOpenSimple} btnColor='#EF6F2B' text={'Open Simple'} textColor='#FFF' />
     </div>
   );
 }
